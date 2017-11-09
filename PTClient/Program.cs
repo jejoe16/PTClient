8934c3d17c95eaac6a867e5bf09cc66f5fe4dad4
Jesper Kragh Jørgensen <jejoe16@student.sdu.dk>
2017-11-09 10:38:12 +0100

    test

--- PTClient/Program.cs
+++ PTClient/Program.cs
@@ -17,6 +17,7 @@ namespace PTClient
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
         }
     }
 }
