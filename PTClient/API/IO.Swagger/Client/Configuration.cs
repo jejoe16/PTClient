4a95a5345e42e7938a7a40dffed3ee63670a7370
Jesper Kragh Jørgensen <jejoe16@student.sdu.dk>
2018-01-20 11:54:54 +0100

    ip

--- PTClient/API/IO.Swagger/Client/Configuration.cs
+++ PTClient/API/IO.Swagger/Client/Configuration.cs
@@ -115,7 +115,7 @@ namespace PTClient.IO.Swagger.Client
         public Configuration()
         {
             UserAgent = "Swagger-Codegen/1.0.0/csharp";
-            BasePath = "http://35.187.75.150:12230/TrackServer";
+            BasePath = "http://192.168.43.251:12230/TrackServer";
             DefaultHeader = new ConcurrentDictionary<string, string>();
             ApiKey = new ConcurrentDictionary<string, string>();
             ApiKeyPrefix = new ConcurrentDictionary<string, string>();
@@ -131,7 +131,7 @@ namespace PTClient.IO.Swagger.Client
             IDictionary<string, string> defaultHeader,
             IDictionary<string, string> apiKey,
             IDictionary<string, string> apiKeyPrefix,
-            string basePath = "http://35.187.75.150:12230/TrackServer") : this()
+            string basePath = "http://192.168.43.251:12230/TrackServer") : this()
         {
             if (string.IsNullOrWhiteSpace(basePath))
                 throw new ArgumentException("The provided basePath is invalid.", "basePath");
